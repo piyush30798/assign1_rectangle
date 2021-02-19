ce6459ecfb226021d45e80f40212a6e7738dd758
Piyush Daga <pdaga7775@conestogac.on.ca>
2021-02-19 11:57:24 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace rectangle
+{
+    public class Rectangle
+    {
+        private int l;
+        private int w;
+
+        public Rectangle()
+        {
+            l = 1;
+            w = 1;
+        }
+
+        public Rectangle(int len, int wid)
+        {
+            l = len;
+            w = wid;
+        }
+
+        public int SetLength(int length)
+        {
+            l = length;
+            return l;
+        }
+        public int GetWidth()
+        {
+            return w;
+        }
+        public int SetWidth(int width)
+        {
+            w = width;
+            return w;
+        }
+        public int GetPerimeter()
+        {
+            return (2 * l + 2 * w);
+        }
+        public int GetArea()
+        {
+            return (l * w);
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
